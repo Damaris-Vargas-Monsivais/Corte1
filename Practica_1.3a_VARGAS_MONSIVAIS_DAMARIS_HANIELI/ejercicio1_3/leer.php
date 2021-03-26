9c2c3168c6005161f366245b0d32d7b255928af5
Damaris Hanieli <dm249833@gmail.com>
2021-03-26 15:06:52 -0600

    Practica 1.3a

--- Practica_1.3a_VARGAS_MONSIVAIS_DAMARIS_HANIELI/ejercicio1_3/leer.php
+++ Practica_1.3a_VARGAS_MONSIVAIS_DAMARIS_HANIELI/ejercicio1_3/leer.php
@@ -1,6 +1,28 @@
-Lorem ipsum
-Es el texto que se usa habitualmente en diseño gráfico en demostraciones de
-tipografías o de borradores de diseño para probar el diseño visual antes de insertar el
-texto final.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<title>Document</title>
+</head>
+<body>
+<?php 
+      $nombreArchivo="lorem.txt";
+	  $archivo=fopen ($nombreArchivo, "r");//abrir archivo
 	  
+	  if($archivo=false){
+		  echo ("Error al abrir archivo");
+		  exit();
+	  }
+	  
+	  $size=filesize($nombreArchivo);//obtener el tamaño del archivo
+	  $elTexto_archivo=fread($archivo, $size);//leemos contenido del archivo
+	  fclose($archivo);//cerrar el archivo
+	  
+	  echo ("File size:$size bytes");
+	  echo ("<pre>$elTexto_archivo</pre>");
+	?>
+</body>
+</html>	  
 	  
